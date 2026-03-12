971b3ac9da0a2a79dd84cfd93cfa6291040e345b
victor <teamogurcic6002@gmail.com>
2026-03-12 19:18:32 +0200

    added part of lab2 - function to check if diagnol is dominant

--- lab2/lib/Math.ex
+++ lab2/lib/Math.ex
@@ -60,20 +60,73 @@ defmodule Math do
 
   def diagonal_dominant?(dominant_elements)
   when is_list(dominant_elements) do
-    set = MapSet.new()
+    # initial indexes of rows in order, that dominant diagnol is possible
+    result = []
 
-    by_columns = dominant_elements
-    |> Enum.group_by(& &1.col)
+    by_columns = dominant_elements|> Enum.group_by(& &1.col)
+    by_rows = dominant_elements |> Enum.group_by(& &1.row)
+
+    result = by_columns
+    |> Enum.reduce([], fn {_col, list}, acc ->
+      cond do
+        Enum.empty?(list) -> acc
+        len(list)==1 -> [Enum.at(list,0).row | acc]
+
+        true ->
+          els_in_rows = list
+          |> Enum.map(fn %Element{row: r, col: c, val: v} ->
+            len(by_rows[r])
+          end)
+
+          single_el_row_idx = Enum.find_index(els_in_rows, fn len -> len == 1 end)
 
 
 
+          case single_el_row_idx do
+            nil ->
+              nil #логика...
+            num ->
+              [el] = by_rows[single_el_row_idx]
+
+              [el|acc]
+          end
+        end
+      end )
+
+
+
+
+
+      def try(col, graph, match_row, visited) do
+        Enum.find_value(graph[col], fn row ->
+          if MapSet.member?(visited, row) do
+            false
+          else
+            visited = MapSet.put(visited, row)
+
+            case match_row[row] do
+              nil ->
+                {:ok, Map.put(match_row, row, col)}
+
+              other_col ->
+                case try(other_col, graph, match_row, visited) do
+                  {:ok, new_match} ->
+                    {:ok, Map.put(new_match, row, col)}
+
+                  _ ->
+                    false
+                end
+            end
+          end
+        end)
+      end
   end
 
 
 
 #после того, как будут мы поймём - может ли доминантные элементы состовлять диагональ, делим все остальные элементы строки на эл этой же строки, состовляющий диагональ.
 #получаем уравнения вида xi = (-ai1/aii)*x1 ... (-ain/aii)xn + bi/aii
-#
+#максимум домингирующих элементов 2 т.к. иначе он нне >= чем сумма всех элементов по модулю
 #
 #
 #
