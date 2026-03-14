176b9c6fec0e3f3e528fb7c5bc63aef161c6c275
victor <teamogurcic6002@gmail.com>
2026-03-15 00:45:57 +0200

    added function 'diagonal_dominant?\1' and test to it and all privios logic

--- lab2/lib/Math.ex
+++ lab2/lib/Math.ex
@@ -47,80 +47,70 @@ defmodule Math do
 
     max_by_row = Enum.group_by(max_elements, & &1.row)
 
-      Enum.with_index(row_abs_sums)
-      |> Enum.all?(fn {row_sum, r} ->
-          case Map.get(max_by_row, r, []) do
-            [] -> true
-            elems ->
-              el = Enum.at(elems, 0)
-              el.val >= row_sum - el.val
-          end
-        end)
+    Enum.with_index(row_abs_sums)
+    |> Enum.all?(fn {row_sum, r} ->
+        case Map.get(max_by_row, r, []) do
+          [] -> true
+          elems ->
+            el = Enum.at(elems, 0)
+            el.val >= row_sum - el.val
+        end
+      end)
   end
 
+  @spec diagonal_dominant?(list(%Element{})) :: %{row::integer() => col::integer()} | false
   def diagonal_dominant?(dominant_elements)
   when is_list(dominant_elements) do
-    # initial indexes of rows in order, that dominant diagnol is possible
-    result = []
 
-    by_columns = dominant_elements|> Enum.group_by(& &1.col)
-    by_rows = dominant_elements |> Enum.group_by(& &1.row)
-
-    result = by_columns
-    |> Enum.reduce([], fn {_col, list}, acc ->
-      cond do
-        Enum.empty?(list) -> acc
-        len(list)==1 -> [Enum.at(list,0).row | acc]
-
-        true ->
-          els_in_rows = list
-          |> Enum.map(fn %Element{row: r, col: c, val: v} ->
-            len(by_rows[r])
-          end)
-
-          single_el_row_idx = Enum.find_index(els_in_rows, fn len -> len == 1 end)
+    graph =
+      dominant_elements
+      |> Enum.group_by(& &1.col)
+      |> Enum.map(fn {c, els} ->
+        {c, Enum.map(els, & &1.row)}
+      end)
+      |> Map.new()
 
 
-
-          case single_el_row_idx do
-            nil ->
-              nil #логика...
-            num ->
-              [el] = by_rows[single_el_row_idx]
-
-              [el|acc]
-          end
-        end
-      end )
-
-
-
-
-
-      def try(col, graph, match_row, visited) do
-        Enum.find_value(graph[col], fn row ->
-          if MapSet.member?(visited, row) do
-            false
-          else
-            visited = MapSet.put(visited, row)
-
-            case match_row[row] do
-              nil ->
-                {:ok, Map.put(match_row, row, col)}
-
-              other_col ->
-                case try(other_col, graph, match_row, visited) do
-                  {:ok, new_match} ->
-                    {:ok, Map.put(new_match, row, col)}
-
-                  _ ->
-                    false
-                end
-            end
-          end
-        end)
+    match_row = %{}
+    Enum.reduce_while(Map.keys(graph), match_row, fn col, match ->
+      case try_assign(col, graph, match, MapSet.new()) do
+        {:ok, new_match} ->
+          {:cont, new_match}
+        :fail ->
+          {:halt, false}
       end
+    end)
   end
+
+
+
+
+
+  defp try_assign(col, graph, match_row, visited) do
+  Enum.reduce_while(graph[col], :fail, fn row, _ ->
+    if MapSet.member?(visited, row) do
+      {:cont, :fail}
+    else
+      visited = MapSet.put(visited, row)
+
+      case Map.get(match_row, row) do
+        nil ->
+          {:halt, {:ok, Map.put(match_row, row, col)}}
+
+        other_col ->
+          case try_assign(other_col, graph, match_row, visited) do
+            {:ok, new_match} ->
+              {:halt, {:ok, Map.put(new_match, row, col)}}
+
+            :fail ->
+              {:cont, :fail}
+          end
+      end
+    end
+  end)
+end
+
+end
 
 
 
@@ -131,4 +121,3 @@ defmodule Math do
 #
 #
 #
-end
--- lab2/test/math_test.exs
+++ lab2/test/math_test.exs
@@ -1,36 +1,158 @@
 defmodule MathTest do
   use ExUnit.Case, async: true
+  alias Math.{Matrix, Element}
 
-  test "get_max_elements_for_each_row/1 returns list" do
-    matrix = %Math.Matrix{rows: 2, columns: 2, data: [[1,2],[3,4]]}
-    expect = [%Math.Element{row: 0,col: 1,val: 2}, %Math.Element{row: 1,col: 1, val: 4}]
+  describe "get_max_elements_for_each_row/1" do
+    test " returns list" do
+      matrix = %Math.Matrix{rows: 2, columns: 2, data: [[1,2],[3,4]]}
+      expected = [%Math.Element{row: 0,col: 1,val: 2}, %Math.Element{row: 1,col: 1, val: 4}]
 
-    actual = Math.get_max_elements_for_each_row(matrix)
+      actual = Math.get_max_elements_for_each_row(matrix)
 
-    assert actual == expect
+      assert actual == expected
 
+    end
   end
 
-  test "elements_dominant?/2 returns true" do
-    matrix = %Math.Matrix{rows: 3, columns: 3, data: [[1,2,0],[3,4,1],[0,0,8]]}
-    max_elements = [%Math.Element{row: 0,col: 1,val: 2}, %Math.Element{row: 1,col: 1, val: 4}, %Math.Element{row: 2,col: 2, val: 8}]
+  describe "elements_dominant?/2" do
+    test "returns true" do
+      matrix = %Math.Matrix{rows: 3, columns: 3, data: [[1,2,0],[3,4,1],[0,0,8]]}
+      max_elements = [%Math.Element{row: 0,col: 1,val: 2}, %Math.Element{row: 1,col: 1, val: 4}, %Math.Element{row: 2,col: 2, val: 8}]
 
-    expect = true
+      expected = true
 
-    actual = Math.elements_dominant?(max_elements, matrix)
+      actual = Math.elements_dominant?(max_elements, matrix)
 
-    assert actual == expect
+      assert actual == expected
+    end
+
+    test "returns false" do
+      matrix = %Math.Matrix{rows: 3, columns: 3, data: [[1,2,0],[3,4,1],[7,7,8]]}
+      max_elements = [%Math.Element{row: 0,col: 1,val: 2}, %Math.Element{row: 1,col: 1, val: 4}, %Math.Element{row: 2,col: 2, val: 8}]
+
+      expected = false
+
+      actual = Math.elements_dominant?(max_elements, matrix)
+
+      assert actual == expected
+    end
   end
 
-  test "elements_dominant?/2 returns false" do
-    matrix = %Math.Matrix{rows: 3, columns: 3, data: [[1,2,0],[3,4,1],[7,7,8]]}
-    max_elements = [%Math.Element{row: 0,col: 1,val: 2}, %Math.Element{row: 1,col: 1, val: 4}, %Math.Element{row: 2,col: 2, val: 8}]
+  describe "diagonal_dominant?/1" do
+    test "return map without double elements in a row" do
+      dominant_elements = [%Math.Element{row: 0,col: 1,val: 7},
+       %Math.Element{row: 1,col: 0, val: 7},
+        %Math.Element{row: 2,col: 2, val: 7}]
 
-    expect = false
+      expected = %{1 => 0, 0=>1, 2=>2}
 
-    actual = Math.elements_dominant?(max_elements, matrix)
+      actual = Math.diagonal_dominant?(dominant_elements)
 
-    assert actual == expect
+      assert actual == expected
+    end
+
+    test "n.1 return map with! double elements in a row" do
+      dominant_elements = [%Math.Element{row: 0,col: 1,val: 7},
+       %Math.Element{row: 1,col: 0, val: 7},
+        %Element{row: 1, col: 1, val: 7},
+         %Math.Element{row: 2,col: 2, val: 7}]
+
+      expected = %{1 => 0, 0=>1, 2=>2}
+
+      actual = Math.diagonal_dominant?(dominant_elements)
+
+      assert actual == expected
+    end
+
+    test "n.2 return map with! double elements in a row" do
+      dominant_elements = [%Math.Element{row: 0,col: 0,val: 7}, %Math.Element{row: 0,col: 2,val: 7},
+       %Math.Element{row: 1,col: 1, val: 7},
+         %Math.Element{row: 2,col: 2, val: 7}]
+
+      expected = %{0 => 0, 1=>1, 2=>2}
+
+      actual = Math.diagonal_dominant?(dominant_elements)
+
+      assert actual == expected
+    end
+
+    test "n.3 return map with! double elements in a row" do
+      #|xx__
+      #|x___
+      #|__x_
+      #|x__x
+
+      dominant_elements = [%Math.Element{row: 0,col: 0,val: 2}, %Math.Element{row: 0,col: 1,val: 7},
+       %Math.Element{row: 1,col: 0, val: 7},
+         %Math.Element{row: 2,col: 2, val: 7},
+          %Element{row: 3, col: 0, val: 7},
+           %Element{row: 3, col: 3, val: 7}]
+
+      expected = %{1=>0, 0=>1, 2=>2, 3=>3}
+
+      actual = Math.diagonal_dominant?(dominant_elements)
+
+      assert actual == expected
+    end
+
+
   end
+test "full path success" do
+      #|x_x__
+      #|_xx__
+      #|__x_x
+      #|x__x_
+      #|___xx
 
+
+      matrix = %Matrix{
+        rows: 5,
+        columns: 5,
+        data: [
+          [7,0,7,0,0],
+          [0,7,7,0,0],
+          [0,0,7,0,7],
+          [7,0,0,7,0],
+          [0,0,0,7,7]
+        ]
+      }
+
+      # dominant_elements =
+      #   [
+      #     %Element{row: 0,col: 0,val: 7}, %Element{row: 0,col: 2,val: 7},
+      #     %Element{row: 1,col: 1, val: 7},%Element{row: 1,col: 2, val: 7},
+      #     %Element{row: 2, col: 2, val: 7},%Element{row: 2, col: 4, val: 7},
+      #     %Element{row: 3,col: 0,val: 7}, %Element{row: 3,col: 3,val: 7},
+      #     %Element{row: 4,col: 3, val: 7},%Element{row: 4,col: 4, val: 7}
+      #   ]
+
+      #expected = %{0=>0, 1=>1, 2=>2, 3=>3, 4=>4} - maybe, but another solution was chosen.
+
+      max_els = matrix
+      |> Math.get_max_elements_for_each_row()
+
+      assert Math.elements_dominant?(max_els,matrix)
+
+      dominant_els = max_els
+
+      actual = dominant_els
+      |> Math.diagonal_dominant?()
+
+
+
+      assert map_size(actual) == matrix.rows
+
+      assert actual
+      |> Map.values()
+      |> Enum.uniq()
+      |> length() == matrix.rows
+
+      assert actual
+      |> Enum.all?(fn {r,c} ->
+        Enum.any?(dominant_els, fn
+          %Element{row: ^r, col: ^c} -> true
+          _ -> false
+        end)
+      end)
+    end
 end
